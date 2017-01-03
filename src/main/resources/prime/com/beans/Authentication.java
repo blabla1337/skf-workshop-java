6fedfe42d1d78e362ace2f9f4373eb93c176cc15
xvass <xvassilakopoulos@twelvesec.com>
2017-01-03 10:01:58 +0200

    Changes

---
+++ src/main/resources/prime/com/beans/Authentication.java
@@ -0,0 +1,28 @@
+package prime.com.beans;
+
+import javax.faces.context.FacesContext;
+
+public class Authentication {
+	  public static final String AUTH_KEY = "app.user.name";
+
+	  private String name;
+	  public String getName() { return name; }
+	  public void setName(String name) { this.name = name; }
+
+	  public boolean isLoggedIn() {
+	    return FacesContext.getCurrentInstance().getExternalContext()
+	        .getSessionMap().get(AUTH_KEY) != null;
+	  }
+
+	  public String login() {
+	    FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put(
+	        AUTH_KEY, name);
+	    return "secret";
+	  }
+
+	  public String logout() {
+	    FacesContext.getCurrentInstance().getExternalContext().getSessionMap()
+	        .remove(AUTH_KEY);
+	    return null;
+	  }
+	}
